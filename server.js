595b458dbe3406f08e5553eb3b095da25f4c74b6
bavshehata <bavshehata@gmail.com>
2021-09-17 20:00:01 +0200

    files

---
+++ server.js
@@ -0,0 +1,54 @@
+//Environment variables
+require("dotenv/config");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const cors = require("cors");
+const app = express(); 
+const port = process.env.PORT ?? 8001;
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+// middleware & static files
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.urlencoded());
+app.use(express.json());
+
+//Constants and variables
+var siteId = "613e0eb5c3defc54432a5756";
+const config = {
+  headers: {
+    Authorization: `Bearer 4182751cd912ec9378c39911fa04d124dfa028819228ee1ee7856a770aa6fe52`,
+    "accept-version": "1.0.0",
+    "content-type": "application/json"
+  },
+};
+var collectionId = "6141d59d547c1823798ee242";
+
+var url, result;
+
+// Get the XML data
+app.get("/data", async (req, res, next) => {
+  url =
+    "https://www.machinefinder.com/dealer_families/9336/machine_feed.xml?key=b562ff30-f65b-0131-e164-005056be003c&password=Deere4";
+  result = await axios.post(url);
+  res.set("Content-Type", "text/xml");
+  res.send(result.data);
+});
+
+// add a webflow item in a collection info
+app.post("/collection/item/add", async (req, res, next) => {
+  try{  
+    url = `https://api.webflow.com/collections/${collectionId}/items`;
+    var data = JSON.stringify({fields: req.body});
+    result = await axios.post(url, data, config);
+    res.send(JSON.stringify(result.data));
+   } catch(e){ 
+     console.log("An error has occured", e);
+   }
+});
